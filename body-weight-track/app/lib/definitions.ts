27c63c9b28995b23c68624a2e76b5192ce9c321d
liy94 <yifei.li@vanderbilt.edu>
2024-06-19 23:57:03 -0500

    added place holder data for body weight

---
+++ body-weight-track/app/lib/definitions.ts
@@ -0,0 +1,4 @@
+export type Entry = {
+  date: Date;
+  weight: number;
+};
---
+++ body-weight-track/app/lib/placeholder-data.js
@@ -0,0 +1,10 @@
+const weights = [
+  {
+    date: new Date("2024-06-01"),
+    weight: 80,
+  },
+  {
+    date: new Date("2024-06-18"),
+    weight: 75,
+  },
+];
