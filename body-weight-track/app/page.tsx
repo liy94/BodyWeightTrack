868de92778009b49dce2265cfd67c2784e7db93a
liy94 <yifei.li@vanderbilt.edu>
2024-06-20 13:47:58 -0500

    added dashboard

--- body-weight-track/app/page.tsx
+++ body-weight-track/app/page.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <p className="text-red-500 md:text-green-500 lg:text-blue-500">Hey</p>;
+  return (
+    <div className="flex">
+      <p className="text-red-500 md:text-green-500 lg:text-blue-500">Hey</p>
+      <Image
+        src="/food.jpeg"
+        width={640}
+        height={853}
+        className="hidden md:block"
+        alt="an image of food"
+      />
+      <Image
+        src="/pig.jpeg"
+        width={360}
+        height={280}
+        className="block md:hidden"
+        alt="an image of pig"
+      />
+    </div>
+  );
 }
