19d88f1a9cc3f16118b320360788e25bcca4a2e8
liy94 <yifei.li@vanderbilt.edu>
2024-07-16 12:10:56 -0500

    sign out button

--- body-weight-track/app/ui/dashboard/TopBar.tsx
+++ body-weight-track/app/ui/dashboard/TopBar.tsx
@@ -1,7 +1,18 @@
+import { signOut } from "@/auth";
+import { Button } from "@mui/material";
+
 export default function TopBar() {
   return (
     <div className="min-w-full bg-blue-500 flex justify-center">
       <h1 className="text-5xl ">Dashboard</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button type="submit">Sign Out</Button>
+      </form>
     </div>
   );
 }
