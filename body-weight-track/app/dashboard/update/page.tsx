190c347e271269e933439cdfd5f48df527e50d20
liy94 <yifei.li@vanderbilt.edu>
2024-07-25 22:44:22 -0500

    fix fetch weight for update page

--- body-weight-track/app/dashboard/update/page.tsx
+++ body-weight-track/app/dashboard/update/page.tsx
@@ -1,9 +1,19 @@
 import CancelButton from "@/app/ui/dashboard/CancelButton";
 import { fetchWeight } from "@/app/lib/data";
 import WeightTable from "@/app/ui/dashboard/WeightTable";
+import { cookies } from "next/headers";
+import { USER_ID } from "@/app/lib/cookieConstants";
+import { fetchWeightByUserID } from "@/app/lib/hongfeiActions";
 
 export default async function Page() {
-  const weights = await fetchWeight();
+  const cookieStore = cookies();
+  const userID = cookieStore.get(USER_ID)?.value;
+
+  if (userID == null) {
+    throw new Error("Oh No! Cannot find userID for update page!");
+  }
+
+  const weights = await fetchWeightByUserID(userID);
 
   return (
     <div>
