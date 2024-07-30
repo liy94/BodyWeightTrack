aad29e3a5db8d1d042bd276355bcf893d92bef1c
liy94 <yifei.li@vanderbilt.edu>
2024-07-29 23:50:32 -0500

    created dialog content

--- body-weight-track/app/ui/dashboard/CRUD.tsx
+++ body-weight-track/app/ui/dashboard/CRUD.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { Button } from "@mui/material";
 import { useState } from "react";
 import CreateDialog from "./CreateDialog";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export default function CRUD() {
   const [createDialogOpen, setCreateDialogOpen] = useState(false);
@@ -11,22 +13,27 @@ export default function CRUD() {
     setCreateDialogOpen(false);
   };
   return (
-    <div className="flex flex-col items-start">
-      <Link key="Create" href="/dashboard/create">
-        <Button>Create New Weight</Button>
-      </Link>
-      <Link key="Update" href="/dashboard/update">
-        <Button>Update Existing Weight</Button>
-      </Link>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="flex flex-col items-start">
+        <Link key="Create" href="/dashboard/create">
+          <Button>Create New Weight</Button>
+        </Link>
+        <Link key="Update" href="/dashboard/update">
+          <Button>Update Existing Weight</Button>
+        </Link>
 
-      <Button
-        onClick={() => {
-          setCreateDialogOpen(true);
-        }}
-      >
-        Create
-      </Button>
-      <CreateDialog open={createDialogOpen} onClose={handleCreateDialogClose} />
-    </div>
+        <Button
+          onClick={() => {
+            setCreateDialogOpen(true);
+          }}
+        >
+          Create
+        </Button>
+        <CreateDialog
+          open={createDialogOpen}
+          onClose={handleCreateDialogClose}
+        />
+      </div>
+    </LocalizationProvider>
   );
 }
--- body-weight-track/app/ui/dashboard/CreateDialog.tsx
+++ body-weight-track/app/ui/dashboard/CreateDialog.tsx
@@ -1,4 +1,10 @@
-import { Dialog } from "@mui/material";
+"use client";
+
+import { Dialog, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs, { Dayjs } from "dayjs";
+import { ChangeEvent, useState } from "react";
+import { Unstable_NumberInput as NumberInput } from "@mui/base/Unstable_NumberInput";
 
 export interface createDialogProps {
   open: boolean;
@@ -7,9 +13,39 @@ export interface createDialogProps {
 
 export default function CreateDialog(props: createDialogProps) {
   const { open, onClose } = props;
+  const [weight, setWeight] = useState<number>(0);
+  const [date, setDate] = useState<Dayjs | null>(dayjs());
+
+  const onDateChange = (newValue: Dayjs | null) => {
+    setDate(newValue);
+  };
+
+  const onWeightChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const newVal = Math.max(0, parseInt(event.target.value));
+    setWeight(newVal);
+  };
+
   return (
     <Dialog open={open} onClose={onClose}>
-      hey
+      <DialogTitle>Log Your Weight</DialogTitle>
+      <DialogContent>
+        <TextField
+          required
+          label="Weight in kg"
+          type="number"
+          sx={{ mt: 1, mb: 1, width: 1 }}
+          value={weight}
+          onChange={onWeightChange}
+        />
+        <DatePicker
+          label="Select Date"
+          value={date}
+          onChange={onDateChange}
+          sx={{ mt: 1, mb: 1, width: 1 }}
+        />
+      </DialogContent>
     </Dialog>
   );
 }
